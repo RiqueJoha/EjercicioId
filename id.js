d1e9301773858f71aeed1dc698237122b1735f11
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-04-20 11:27:55 -0300

    Ejercicioid

---
+++ id.js
@@ -0,0 +1,14 @@
+const imagenPerrito = document.querySelector ("#img-perrito");
+const tituloResumen = document.querySelector ("#titulo-resumen");
+const parrafo = document.querySelector ("#parrafo");
+const enlace = document.querySelector ("#enlace");
+const cuerpoPagina = document.querySelector ("body");
+
+imagenPerrito.src="https://th.bing.com/th/id/OIP.Ap_ApmXf7nS7lnfBWJqqrAHaGs?pid=ImgDet&rs=1";
+tituloResumen.innerHTML= "AMAMOS A LOS PITBULL";
+parrafo.innerHTML= "El mundo de los perros";
+enlace.innerHTML= "Haz click para mas información";
+enlace.href= "https://en.wikipedia.org/wiki/Pit_bull";
+cuerpoPagina.style.backgroundColor= "pink";
+/* TITULO_RESUMEN. style.color="green"; */
+CUERPOPAGINA.style.color= "green"
